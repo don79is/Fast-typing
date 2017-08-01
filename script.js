2e2bb919d04c7ae9f92eae4ebe0189b253ba2764
don79is <don79is@gmail.com>
2017-08-01 10:10:23 +0300

    add functios

---
+++ script.js
@@ -0,0 +1,57 @@
+/**
+ * Created by Donatas Tumanas on 2017-08-01.
+ */
+
+
+var fastTyping = function ()
+{
+
+var REGISTER = 'reg',
+    LEVEL_SELECTION = 'level',
+    GAME = 'game',
+    GAME_OVER = 'game_over';
+
+function changeState(value) {
+
+    switch (value){
+        case  REGISTER:
+            break;
+        case LEVEL_SELECTION:
+            break;
+        case GAME:
+            break;
+        case GAME_OVER:
+            break;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    function initialize() {
+console.log()
+    }
+}
