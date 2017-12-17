cea61c0b05f6cce485c0c909de23a98de33e56f1
Donatas Tumanas <donatastum@gmail.com>
2017-12-17 22:56:12 +0200

    add css corect js

--- script.js
+++ script.js
@@ -1,280 +1,287 @@
-/**
- * Created by Donatas Tumanas on 2017-08-01.
- */
 var FastTyping = function () {
+    const STATE_REGISTER = 'register';
+    const STATE_LEVEL_SELECTION = 'choose_level';
+    const STATE_GAME = 'game';
+    const STATE_GAME_OVER = 'game_over';
 
-    var STATE_REGISTER = 'reg',
-        STATE_LEVEL = 'level',
-        STATE_GAME = 'game',
-        STATE_GAME_OVER = 'game_over';
-    var name;
-    var last_state;
+    var name,                                               //for later use in RegisterLogics
+        lastState,                                           //for later use in switch
+        level,
+        game_time,
+        start_time,
+        score = 0,
+        ScoreData,
+        duration,
+        average_speed;
 
-    var RegisterLogic = function () {
+    //---------------------------------------------------- Register ---------------------------------------------------------
 
-        var view = $('#register');
-        var input = $('#first-register');
-        var button = $('#btn-go');
+    var RegisterLogics = function () {
+        var view = $('#register');                      //jquery object created, now we can use jquery functions
+        var input = $('#name');
+        var button = $('#go');
 
-
-        this.show = function () {
-
-            view.removeClass('hidden');
+        this.show = function () {                       //it's possible tu use it like public because of THIS
+            view.removeClass('hidden');                 //removes hidden class (from bootstrap) that you could see the view
             enable();
         };
 
-        this.hide = function () {
+        this.hide = function () {                       //removed before, now we need to add the same class
             view.addClass('hidden');
-            disabled();
+            disable();
         };
 
-        function enable() {
+        /**
+         * will be checking the value.length of input and will activate the button after
+         */
+        function enable()                               //will be used with show cuz it enables the button after
+        {
             input.keyup(function () {
-                if (input.val().length > 3) {
-                    button.attr('disabled', false).removeClass("btn btn-danger").addClass("btn btn-success");
-
+                if (input.val().length >= 3) {
+                    button.attr('disabled', false);
                 } else {
-                    button.attr('disabled', true).removeClass("btn btn-success").addClass("btn btn-danger");
+                    button.attr('disabled', true);
                 }
-                button.click(function () {
-                    name = input.val();
-                    changeState(STATE_LEVEL);
-
-                })
             });
 
+            button.click(function () {
+                name = input.val();
+                changeState(STATE_LEVEL_SELECTION)
+            });
         }
 
-        function disabled() {
-
+        function disable() {
             input.unbind();
             button.unbind();
-            input.val("");
+            input.val('');
         }
     };
-    var register = new RegisterLogic();
+
+    /** initializing object. now u can call it with this variable. it's prepared for using in switch.
+     * possible to create object only after describing function name.
+     * @type {RegisterLogics}
+     */
+    var register = new RegisterLogics();
+
+    //---------------------------------------------------------- Select Level ----------------------------------------------------------------
+
+    var LevelSelectionLogics = function () {
+        var view = $('#level-select');                                                         //jquery object created, now we can use jquery functions
+        var button = $('#play');
 
 
-    var LevelLogic = function () {
-
-        var view = $('#level-select');
-        var input = $('#btn-level');
-
-        this.show = function () {
-            view.removeClass('hidden').prepend('<h4>' + name + '</h4>');
-            selectDificulty()
+        this.show = function () {                                                        //it's possible tu use it like public because of THIS
+            view.removeClass('hidden');                                                  //removes hidden class (from bootstrap) that you could see the view
+            $('#gamerName').html(name);
+            enable();
         };
 
-        this.hide = function () {
-
+        this.hide = function () {                                                   //removed before, now we need to add the same class
             view.addClass('hidden');
-            disableLevel()
-
+            disable();
         };
-        function selectDificulty() {
 
-            input.click(function () {
-                level = $('input[name=play]:checked').val();
-                changeState(STATE_GAME);
+        /** var level with checkbox value defined by them names;
+         * enables chosen inputs value and changes state
+         */
+        function enable() {
+            button.click(function () {
+                level = $('input[name = optionsRadios]:checked').val();
+                changeState(STATE_GAME)
             });
-
         }
 
-        function disableLevel() {
-            input.unbind();
-            // level = $('input[name = gamePlay]').val('');
+        function disable() {
+            button.unbind();
         }
-
     };
-    var level = new LevelLogic();
 
+    var levelSelection = new LevelSelectionLogics();
 
-    var GameLogic = function () {
+    //---------------------------------------------------------------- THE GAME ----------------------------------------------------------
 
-        var view = $('#game');
-        var letters = 'abcdefghijklmnopqrstuvwxyz',
-            timeOut,
+    var GameLogics = function () {
+        var view = $('#game'),
+            letters = 'abcdefghijklmnprstuvwxyz',
+            gamer = $('#gamer'),
+            letter_show  = $('#point'),
+            is_golden,
+            end_time,
+            letter_click,
+            userInput = true,
             letterKey,
-            letterShow = $('h2'),
-            lifeCount,
-            userInput,
-            gamer =$('#gamer'),
-            letterAppearance,
-            keyUpTime,
-            timeAmount,
-            isGolden;
+            timeOut,
+            letter_show_now,
+            amount,
+            livesCount;
 
 
         this.show = function () {
-
-            gamer.html(name);
-            lifeCount = 3;
-            $('#life').html(lifeCount);
-            userInput = true;
+            view.removeClass('hidden');
+            livesCount = 3;
             score = 0;
-            view.removeClass('hidden').prepend('<h6>' + name + ' have ' + level + ' s</h6>');
+            gamer.html(name);
             changeLetter();
             enable();
-
         };
+
         this.hide = function () {
-
             view.addClass('hidden');
-            disabled();
-
+            disable();
         };
 
         function updateScore() {
 
-            if (isGolden) {
-                isGolden = false;
-                for (i = 0; i < 5; i++) {
+            if ((score % 20) === 0 && score !== 0) {
+                livesCount += 1;
+                $('#live').html(livesCount);
+            }
+            if (is_golden) {
+                is_golden = false;
+                for (var i = 0; i < 5; i++) {
                     updateScore();
                 }
             } else {
                 score += 1;
             }
-
-            if (score % 20 === 0) {
-
-                lifeCount += 1;
-                $('#life').html(lifeCount);
-            }
-
             $('#score').html(score);
+
+        }
+
+        /**
+         * if live is 0, redirects to game over state
+         */
+
+        function removeLive() {
+
+            livesCount -= 1;
+            $('#live').html(livesCount);
+
+            if (livesCount === 0) {
+                end_time = Date.now();
+                changeState(STATE_GAME_OVER);
+            }
         }
 
         function enable() {
+
+            $(window).keydown(
+                function () {
+                    letter_show.addClass('active')
+                });
+
             $(window).keyup(
                 function (e) {
-
+                    letter_show.removeClass('active')
                     if (e.key === letters[letterKey]) {
-                        updateScore()
+                        updateScore();
                     } else {
-                        removeLife()
+                        removeLive();
                     }
+                    letter_click = Date.now();
 
-                    keyUpTime = Date.now();
 
                     userInput = true;
                     changeLetter();
-
-                    timeAmount = (letterAppearance - keyUpTime);
-                    console.log(keyUpTime, letterAppearance, timeAmount);
-
-                }
-            )
+                })
         }
-
-        function removeLife() {
-
-            lifeCount -= 1;
-            $('#life').html(lifeCount);
-
-            if (lifeCount <= 0)
-                changeState(STATE_GAME_OVER);
+        function disable() {
+            $(window).unbind();
+            clearTimeout(timeOut);
         }
 
         function changeLetter() {
 
 
             if (!userInput) {
-                removeLife();
+                removeLive()
             }
             clearTimeout(timeOut);
 
-
-            if (lifeCount <= 0) {
+            if (livesCount <= 0)
                 return;
 
-            }
             if (Math.random() < 0.1) {
-                isGolden = true;
-                letterShow.addClass('golden');
-
+                is_golden = true;
+                letter_show.addClass('golden');
             } else {
-                isGolden = false;
-                letterShow.removeClass('golden');
+                is_golden = false;
+                letter_show.removeClass('golden');
             }
+
             userInput = false;
+            letterKey = Math.round(Math.random() * (letters.length - 1))
+            letter_show.html(letters[letterKey]);
+            letter_show_now = Date.now();
             timeOut = setTimeout(changeLetter, level * 1000);
-            letterKey = Math.round(Math.random() * (letters.length - 1));
-            letterShow.html(letters[letterKey]);
-
-            letterAppearance = Date.now();
         }
-
-        function disabled() {
-            $(window).unbind();
-            clearTimeout(timeOut);
-        }
-console.log(letters);
-
     };
-    var game = new GameLogic();
 
-    var GameOverLogic = function () {
+    var game = new GameLogics();
 
-        var view = $('#game-over');
-        var input = $('#btn-over');
+    //-------------------------------------------------------------- Game Over -------------------------------------------------
+
+    var GameOverLogics = function () {
+
+        var view = $('#over');
+        var lastText = 'Oops.. you just lost the game:)';
+        var again = $('#again');
 
         this.show = function () {
-
             view.removeClass('hidden');
-            // $('#final-score').html(score);
-
 
         };
+
         this.hide = function () {
-
-            view.addClass('hidden');
-            disabled();
-
+            view.addClass('hidden')
         };
 
-        input.click(function () {
-            changeState(STATE_REGISTER);
-        });
-        function disabled() {
-            // name = '';
-            // score = 0;
-            // level = null;
 
-        }
+        $('#textOver').html(lastText);
+
+        again.click(function () {
+            changeState(STATE_REGISTER)
+        });
     };
-    var game_over = GameOverLogic();
+
+    var gameOver = new GameOverLogics();
+
+    //----------------------------------------------------------------- Changing a state ---------------------------------------
 
     function changeState(value) {
-        if (last_state) {
-            last_state.hide()
+
+        if (lastState) {
+            lastState.hide();
         }
+
         switch (value) {
-            case  STATE_REGISTER:
-                last_state = register;
+            case STATE_REGISTER:
+                lastState = register;                        //register was created before as object and we're calling it with lastState for using show() later.
 
                 break;
 
-            case STATE_LEVEL:
-                last_state = level;
+            case STATE_LEVEL_SELECTION:
+
+                lastState = levelSelection;                    //new state requires new last state value, otherwise it will show te same (register)
 
                 break;
 
             case STATE_GAME:
-                last_state = game;
-
+                lastState = game;
                 break;
 
             case STATE_GAME_OVER:
-                last_state = game_over;
-
+                lastState = gameOver;
                 break;
         }
 
-        last_state.show();
-        console.log(last_state);
+        lastState.show();
+        console.log('changing', lastState);
     }
 
 
-
+    /**
+     * initializing first state
+     */
     changeState(STATE_REGISTER);
 };
